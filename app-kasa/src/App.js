02c98d80a8dda40ecec7a03e17fa09c30d6c7ea0
margaux Doisy <margauxdoisy@gmail.com>
2023-08-18 18:33:03 +0200

    commit -m "ajout router-pages"

--- app-kasa/src/App.js
+++ app-kasa/src/App.js
@@ -1,6 +1,25 @@
-const App = () =>{
-return(
-  <h1>Hello My app Kasa</h1>
-)
-}
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import FicheLogement from "./pages/FicheLogement";
+import Erreur404 from "./pages/Erreur404";
+import APropos from "./pages/APropos";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/FicheLogement" element={<FicheLogement />} />
+        <Route path="/Erreur404" element={<Erreur404 />} />
+        <Route path="/APropos" element={<APropos />} />
+
+      </Routes>
+    </BrowserRouter>
+    
+  );
+};
+
 export default App;
+
+//creer un router//
