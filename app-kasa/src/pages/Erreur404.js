3f498bcbbefd096c513c8c92202eb88b1190be0b
margaux Doisy <margauxdoisy@gmail.com>
2023-08-29 23:31:19 +0200

    commit -m "erreur 404"

--- app-kasa/src/pages/Erreur404.js
+++ app-kasa/src/pages/Erreur404.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
 import LogoFooter from '../components/LogoFooter';
+import { Link } from 'react-router-dom';
 
 
 
@@ -12,7 +13,11 @@ const Erreur404 = () => {
                 <Logo/>
                 <Navigation/>
             </div>
-                <h1>Erreur 404</h1>
+            <div className='container-404'>
+                <h1 className='title-404'>Erreur 404</h1>
+                <p className='texte-404'>Oups! La page que vous demandez n'existe pas.</p>  
+                <Link to='/'>Retourner sur la page d'accueil</Link>
+            </div>
                <LogoFooter/>
         </div>
     );
