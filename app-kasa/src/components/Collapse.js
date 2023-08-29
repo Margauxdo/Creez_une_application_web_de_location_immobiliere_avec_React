f5203ff5e6f6b221e2090758f096e93b42e80c36
margaux Doisy <margauxdoisy@gmail.com>
2023-08-29 15:23:20 +0200

    commit -m "css a propos"

--- app-kasa/src/components/Collapse.js
+++ app-kasa/src/components/Collapse.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import '../styles/components/collapse.css';
 
-const Collapse = ({title}) => {
+const Collapse = ({title, children}) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleCollapse = () => {
         setIsOpen(!isOpen);
     }
+
+    const chevronClass = isOpen ? 'collapse-chevron up' : 'collapse-chevron down';
+
+
     return (
         <div className="collapse">
       <button onClick={toggleCollapse} className="collapse-button">
         {title}
+        <i className={chevronClass}></i>
       </button>
-      {isOpen && <div className="collapse-content">Contenu de {title}</div>}
+      {isOpen && <div className="collapse-content">{children}</div>}
     </div>
     );
 };
