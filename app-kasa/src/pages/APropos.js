f92f0fb2869ecb31050386e60ee078ce4d5aa6b5
margaux Doisy <margauxdoisy@gmail.com>
2023-11-04 21:45:19 +0100

    commit -m "erreur css"

--- app-kasa/src/pages/APropos.js
+++ app-kasa/src/pages/APropos.js
@@ -18,28 +18,25 @@ const APropos = () => {
             </div>
             <div className="collapse">
                 <div className='container'>
-                <Collapse >
-                 <div class="titleCollapse"><h3>Fiabilité</h3></div>   
-                <div class="texteCollapse"><p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p></div>
+                <Collapse  title="Fiabilité">
+                  
+                <div className="texteCollapse"><p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p></div>
                 </Collapse>
                 </div>
                 <div className='container'>
-                <Collapse >
-                <div class="titleCollapse"><h3>Respect</h3></div>   
-                <div class="texteCollapse"><p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p></div>
+                <Collapse title="Respect"> 
+                <div className="texteCollapse"><p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p></div>
                 </Collapse>
                 </div>
                 <div className='container'>
-                <Collapse >
-                <div class="titleCollapse"><h3>Service</h3></div>   
-                <div class="texteCollapse"><p> Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question..</p></div>
+                <Collapse title="Service"> 
+                <div className="texteCollapse"><p> Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question..</p></div>
 
                 </Collapse>
                 </div>
                 <div className='container'>
-                <Collapse >
-                <div class="titleCollapse"><h3>Sécurité</h3></div>   
-                <div class="texteCollapse"><p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p></div>
+                <Collapse title="Sécurité" >  
+                <div className="texteCollapse"><p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p></div>
 
                 
                 </Collapse>
